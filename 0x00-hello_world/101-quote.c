c7c875e623fabfe6dab1b944fc31e10367344f01
segun <70609367+OluwasegunO-CTRL@users.noreply.github.com>
2022-06-03 19:31:40 +0100

    add now

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ *
+ * Description: using the main function
+ * this program prints out with proper grammer,
+ * but the outcome is a piece of art,
+ * Return: 1
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
