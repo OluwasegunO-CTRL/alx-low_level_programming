0db16fada71a627d7d5b4002a3bbe6e5b7da9fa7
segun <70609367+OluwasegunO-CTRL@users.noreply.github.com>
2022-06-06 03:39:48 +0100

    soooo

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - the main function print string in putchar
+ *
+ * Description: print alphabet
+ * Return: 0
+ */
+
+int main(void)
+{
+int c;
+
+for (c = 0; c < 10; c++)
+{
+
+	printf("%d", c);
+}
+
+printf("\n");
+return (0);
+}
