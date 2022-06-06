7f1927e234663b1827dbd88261d428c03828ae56
segun <70609367+OluwasegunO-CTRL@users.noreply.github.com>
2022-06-06 04:48:24 +0100

    adds

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - the main function print string in putchar
+ *
+ * Description: print alphabet
+ * Return: 0
+ */
+
+int main(void)
+{
+char c;
+
+for (c = '0'; c <= '9'; c++)
+{
+
+	putchar(c);
+}
+
+putchar('\n');
+return (0);
+}
