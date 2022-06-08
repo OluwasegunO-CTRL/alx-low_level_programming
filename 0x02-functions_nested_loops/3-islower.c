e247a24839782cc3ea8cafd09286efd7db69a60e
segun <70609367+OluwasegunO-CTRL@users.noreply.github.com>
2022-06-08 22:43:18 +0100

    _islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - returns 1 if if letter is lowercase, 0 if not.
+ * @c: char type letter
+ * Return: 1 if lowercase, 0 if not lowercase
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+
+	else 
+		return (0);
+}
