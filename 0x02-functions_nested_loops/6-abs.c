b426d0976f5131ebff06a4892616cafc9ba23617
segun <70609367+OluwasegunO-CTRL@users.noreply.github.com>
2022-06-09 00:32:54 +0100

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - print absolute value of a number
+ *@n: int type number
+ *Return: absolute value of @n
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+
+	else
+	{
+		return (n);
+	}
+}
+
