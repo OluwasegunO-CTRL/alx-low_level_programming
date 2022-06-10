c72c813fa3135b284efac22a2211c34ef55f0cb2
segun <70609367+OluwasegunO-CTRL@users.noreply.github.com>
2022-06-10 05:30:05 +0100

    print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_line - draw diagonal line
+ * @n: number of times underscore is printed
+ *Description: can only use _putchar to rrpint
+ */
+
+void print_diagonal(int n)
+{
+	int c, i;
+
+	c = 0;
+	while (n > 0)
+	{
+		i = c;
+		while (i > 0)
+		{
+			_putchar(' ');
+			i--;
+		}
+		_putchar('\\');
+		_putchar('\n');
+		c++;
+		n--;
+
+	}
+	if (c < 1)
+		_putchar('\n');
+}
