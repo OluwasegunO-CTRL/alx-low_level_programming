9cf156473a0d37239836f5306ad299c551361418
segun <70609367+OluwasegunO-CTRL@users.noreply.github.com>
2022-06-10 04:34:59 +0100

    add

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,7 +4,7 @@ int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
-int print_most_numbers(int);
+void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 int print_diagonal(int n);
