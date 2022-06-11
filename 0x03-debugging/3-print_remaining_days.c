8a055ffb3a66702bf06cfbf5d22cfefb63f4e92e
segun <70609367+OluwasegunO-CTRL@users.noreply.github.com>
2022-06-11 04:48:35 +0100

    add

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -10,9 +10,9 @@
  * Return: void
  */
 
-int print_remaining_days(int month, int day, int year)
+void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
+	if (year % 400 == 0 || (year % 4== 0 && !(year % 100 == 0)))
 	{
 		if (month >= 2)
 		{
