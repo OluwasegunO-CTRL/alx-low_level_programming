d9c25a82bee29e38507e8955407dca7582f8fe8a
segun <70609367+OluwasegunO-CTRL@users.noreply.github.com>
2022-06-24 03:43:25 +0100

    function return value of x raised to power y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - return value of x raised to power y.
+ * @x: int type
+ * @y: int type
+ * Return: ffff
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y > 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else if (y == 1)
+		return (x);
+
+	return (x * _pow_recursion(x, y - 1));
+}
+
